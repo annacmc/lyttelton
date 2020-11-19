d1fe058058d177edddc0bad4009d032ab5f96831
Anna McPhee <30754158+annacmc@users.noreply.github.com>
2020-11-20 01:05:24 +1300

    add bootstrap navwalker

--- header.php
+++ header.php
@@ -47,13 +47,20 @@
 		</div><!-- .site-branding -->
 
 
-		<nav id="site-navigation" class="main-navigation">
+		<nav id="site-navigation" class="main-navigation navbar navbar-expand-lg navbar-light bg-light">
 			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'lyttelton' ); ?></button>
 			<?php
 			wp_nav_menu(
 				array(
 					'theme_location' => 'menu-1',
 					'menu_id'        => 'primary-menu',
+					'depth'           => 2,
+					'container'       => 'div',
+					'container_class' => 'collapse navbar-collapse',
+					'container_id'    => 'bs-example-navbar-collapse-1',
+					'menu_class'      => 'nav navbar-nav',
+					'fallback_cb'     => 'WP_Bootstrap_Navwalker::fallback',
+					'walker'          => new WP_Bootstrap_Navwalker(),
 				)
 			);
 			?>
